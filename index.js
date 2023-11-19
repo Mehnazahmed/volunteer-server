873c8bdc681f93e1eb808803f3d0de0f9efc6e1c
Mehnaz <mehnazchowdhury613@gmail.com>
2023-11-19 14:13:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port =process.env.PORT || 5000;
+//middleware
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sn1j5xu.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    
+   
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+app.get('/',async(req,res)=>{
+    res.send('Volunteer is running');
+});
+app.listen(port,()=>console.log(`Volunteer running on ${port}`));
