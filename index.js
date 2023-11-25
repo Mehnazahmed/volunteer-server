aa1cfdfbec9a62ccfcc8efc45b20d3e8cb66f7a6
Mehnaz <mehnazchowdhury613@gmail.com>
2023-11-25 23:25:56 +0600

    server site fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port =process.env.PORT || 5000;
 //middleware
@@ -19,7 +19,60 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
+    const works = client.db('volunteerDB').collection('works');
+    const users = client.db('volunteerDB').collection('users');
+    const events = client.db('volunteerDB').collection('events');
     
+    
+
+    app.get('/works',async(req,res)=>{
+      const result = await works.find().toArray();
+      res.send(result);
+    });
+    app.get('/users',async(req,res)=>{
+      const result = await users.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/users',async(req,res)=>{
+      const user =req.body;
+      const query ={email: user.email};
+      const existingUser =await users.findOne(query);
+      if(existingUser){
+        return res.send({message:'user already exists'});
+      }
+
+      const result=await users.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete('/users/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result =await users.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get('/events',async(req,res)=>{
+      const result =await events.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/events',async(req,res)=>{
+      const event =req.body;
+      const result =await events.insertOne(event);
+      res.send(result);
+    });
+
+    app.delete('/events/:id',async(req,res)=>{
+    const id= req.params.id;
+    const query={_id: new ObjectId(id)};
+    const result = await events.deleteOne(query);
+    res.send(result);
+    });
+
+    
+
    
   } finally {
     
