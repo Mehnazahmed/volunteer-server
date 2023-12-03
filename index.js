c050451622cebfafaee45f423f1a2a99a8fc1510
Mehnaz <mehnazchowdhury613@gmail.com>
2023-12-03 13:32:17 +0600

    admin authentication

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require("jsonwebtoken");
+
+
+const verifyJWT =(req,res,next)=>{
+  const authorization =req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true,message:'unauthorized access'});
+  }
+
+  const token =authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({error:true,message:'Unauthorized access'});
+    }
+    req.decoded=decoded;
+    next();
+  });
+
+
+};
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port =process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sn1j5xu.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -17,11 +43,45 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
+
+
 async function run() {
   try {
     const works = client.db('volunteerDB').collection('works');
     const users = client.db('volunteerDB').collection('users');
     const events = client.db('volunteerDB').collection('events');
+
+    //jwt
+    app.post('/jwt',async(req,res)=>{
+      const user= req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN,{expiresIn:'60s'});
+      res.send({token});
+    });
+
+    const verifyAdmin =async(req,res,next)=>{
+      const email =req.decoded.email;
+      const query ={email:email};
+      const user = await users.findOne(query);
+      if(user?.role !== 'admin'){
+        return res.status(403).send({error:true, message:'Forbidden message'});
+      }
+      next();
+
+    };
+
+    app.get('/users/admin/:email',verifyJWT, async(req,res)=>{
+      const email =req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin:false});
+      }
+
+      const query ={email:email}
+      const user =await users.findOne(query);
+      const result ={admin: user?.role ==='admin'};
+      res.send(result);
+    });
     
     
 
@@ -29,7 +89,7 @@ async function run() {
       const result = await works.find().toArray();
       res.send(result);
     });
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyJWT,verifyAdmin,async(req,res)=>{
       const result = await users.find().toArray();
       res.send(result);
     });
